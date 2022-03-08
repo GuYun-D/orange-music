94bc499f7e61674d9016ee75caaafc2c6562acd2
GuYun-D <1946306647@qq.com>
2022-03-08 16:50:24 +0800

    封装request函数，获取渲染轮播图

---
+++ utils/request.js
@@ -0,0 +1,20 @@
+// 封装ajax
+export default function (url, data, method = "GET", cb) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url,
+      data,
+      method,
+      success: (result) => {
+        resolve(result.data)
+      },
+      fail: (errorMsg) => {
+        console.log(errorMsg);
+        reject(errorMsg)
+      },
+      complete: () => {
+        cb && typeof cb === "function" && cb()
+      }
+    })
+  })
+}
