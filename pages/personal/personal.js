81c2fe776136409d4a6b222aea92c6ed4e6a1bd4
GuYun-D <1946306647@qq.com>
2022-03-09 13:40:01 +0800

    制作用户下拉动画

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,65 +1,66 @@
-// pages/personal/personal.js
+let startY = 0 // 手指起始坐标
+let moveY = 0 // 手指移动实时坐标
+let moveDistance = 0 // 手指移动的距离
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    transformYNum: "translate(0)",
+    transitionNum: ""
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  handleTouchStart(event) {
+    startY = event.touches[0].clientY
+    this.setData({
+      transitionNum: ""
+    })
+  },
+
+  handleTouchMove(event) {
+    moveY = event.touches[0].clientY
+
+    // 计算手指移动的距离
+    moveDistance = moveY - startY
+    
+    if(moveDistance < 0){
+      return
+    }
+
+    if(moveDistance >= 80){
+      moveDistance = 80
+    }
+
+    this.setData({
+      transformYNum: `translateY(${moveDistance}rpx)`
+    })
+  },
+
+  handleTouchEnd() {
+    this.setData({
+      transformYNum: 'translateY(0rpx)',
+      transitionNum: "transform 350ms linear"
+    })
+  },
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
 
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
 
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
   }
