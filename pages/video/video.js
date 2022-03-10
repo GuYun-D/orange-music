87f6763f3a48ea030d361fec5a37a3f600d846c0
GuYun-D <1946306647@qq.com>
2022-03-10 10:09:18 +0800

    视频导航区的搭建

--- pages/video/video.js
+++ pages/video/video.js
@@ -1,66 +1,26 @@
-// pages/video/video.js
+import request from '../../utils/request'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    navList: [],
+    navId: ""
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.getNavList()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  async getNavList() {
+    const navResult = await request('video/group/list')
+    this.setData({
+      navList: navResult.data.slice(0, 14),
+      navId: navResult.data[0].id
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点击修改id
+  chnageNav(event) {
+   let navId = event.currentTarget.id
+    this.setData({
+      navId
+    })
   }
 })
